85d0c9cfd354f5ea96119ebc681171933e7c464d
legop3 <minecratfd8@gmail.com>
2025-11-15 04:13:18 -0500

    oopsie

--- webui/src/lib/socket.js
+++ webui/src/lib/socket.js
@@ -3,7 +3,8 @@ import { io } from "socket.io-client";
 console.log('socket.io client loaded')
 
 const serverUrl = import.meta.env.VITE_ROVERD_URL ?? "http://localhost:8080";
-export const socket = io(serverUrl, {
+export const socket = io({
+// export const socket = io(serverUrl, {
   transports: ["websocket"], // skip polling if your backend supports it
   query: {
     role: "user"
