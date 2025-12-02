0da5d72d91639529bbdf316d4cc26a15a0e486ff
legop3 <minecratfd8@gmail.com>
2025-12-02 00:29:31 -0500

    try websockets only...

--- webui/src/lib/socket.js
+++ webui/src/lib/socket.js
@@ -4,7 +4,7 @@ const configured = import.meta.env.VITE_ROVERD_URL?.trim();
 const resolvedUrl = configured && configured.length > 0 ? configured : window.location.origin;
 console.info('[socket] connecting to', resolvedUrl);
 export const socket = io(resolvedUrl, {
-  transports: ['polling', 'websocket'],
+  transports: ['websocket'],
   timeout: 15000,
 });
 socket.on('connect_error', (err) => console.error('connect_error', err.code, err.message, err.data));
