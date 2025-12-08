57d4b5a415c8b95d4935ea916d1b46009bdd2525
legop3 <minecratfd8@gmail.com>
2025-12-08 14:03:38 -0500

    shorter beeps

--- webui/src/controls/constants.js
+++ webui/src/controls/constants.js
@@ -14,7 +14,7 @@ export const COMMAND_DELAY_MS = 100;
 
 export const SONG_NOTE_RANGE = [31, 127];
 export const SONG_DEFAULT_NOTE = 60;
-export const SONG_DEFAULT_DURATION = 32;
+export const SONG_DEFAULT_DURATION = 8;
 export const SONG_REPEAT_MS = 150;
 
 export const OI_COMMANDS = {
