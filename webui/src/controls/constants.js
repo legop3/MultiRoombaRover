d64e99b674a8056b7eed8e80e296d9a320179e8e
legop3 <minecratfd8@gmail.com>
2026-01-31 16:30:50 -0500

    more adjustment

--- webui/src/controls/constants.js
+++ webui/src/controls/constants.js
@@ -18,9 +18,9 @@ export const SONG_DEFAULT_DURATION = 8;
 export const SONG_REPEAT_MS = 250;
 
 export const HORN_MAX_MS = 1200;
-export const HORN_HEAT_UP_PER_SEC = 1.3;
-export const HORN_HEAT_COOL_PER_SEC = 0.35;
-export const HORN_HEAT_RESUME_THRESHOLD = 0.7;
+export const HORN_HEAT_UP_PER_SEC = 1;
+export const HORN_HEAT_COOL_PER_SEC = 0.2;
+export const HORN_HEAT_RESUME_THRESHOLD = 0.8;
 
 export const OI_COMMANDS = {
   start: [128],
